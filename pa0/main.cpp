e4ed0d26cc675a4bb43301238e122af7e1e283c3
tangxiaolong <xltangcs@outlook.com>
2023-05-15 19:09:20 +0800

    Signed-off-by: tangxiaolong <xltangcs@outlook.com>

---
+++ pa0/main.cpp
@@ -0,0 +1,32 @@
+#include<cmath>
+#include<Eigen/Core>
+#include<Eigen/Dense>
+#include<iostream>
+
+using namespace std;
+using namespace Eigen;
+
+int main() {
+    const float PI = 3.1415926;
+    float ra =  PI / 4.0;
+    float tx = 1.0f;
+    float ty = 2.0f;
+    Vector3f v(2.0f, 1.0f, 1.0f);
+
+    Matrix3f r, t;
+    r << cos(ra), -sin(ra), 0.0, 
+         sin(ra), cos(ra), 0.0f, 
+         0.0f, 0.0f, 1.0f;
+
+    t << 1.0f, 0.0f, tx,
+         0.0f, 1.0f, ty, 
+         0.0f, 0.0f, 1.0f;
+
+    cout << t * r * v;
+
+
+
+
+
+    return 0;
+}
